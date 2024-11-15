5a66421f2c61699cbab52ec6068ad7b747d69599
silmidina <silmirhmdn08@gmail.com>
2024-11-16 03:31:49 +0700

    Handling Alerts menggunakan 2 button ok and close part 2

--- HandlingAlerts.py
+++ HandlingAlerts.py
@@ -15,6 +15,13 @@ driver.get("https://demoqa.com/alerts")
 time.sleep(3)
 
 #Temukan elemen menggunakan By
-driver.find_element(By.ID, "alertButton").click()
+#driver.find_element(By.ID, "alertButton").click()
+#time.sleep(3)
+#driver.switch_to.alert.accept()
+
+driver.find_element(By.ID, "confirmButton").click()
 time.sleep(3)
-driver.switch_to.alert.accept()
+#confirm alert ok
+#driver.switch_to.alert.accept()
+#confirm alert close
+driver.switch_to.alert.dismiss()
