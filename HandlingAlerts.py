d38f78c386eb89334803587785ec0c0d17f69074
silmidina <silmirhmdn08@gmail.com>
2024-11-16 03:17:53 +0700

    Handling Alerts part 1

---
+++ HandlingAlerts.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+#Buat opsi untuk Chrome
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+#Inisialisasi driver
+driver = webdriver.Chrome(options=chrome_options)
+
+#Buka URL
+driver.get("https://demoqa.com/alerts")
+time.sleep(3)
+
+#Temukan elemen menggunakan By
+driver.find_element(By.ID, "alertButton").click()
+time.sleep(3)
+driver.switch_to.alert.accept()
