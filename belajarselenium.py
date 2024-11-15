11603777eb46777f1cf45f61890412d5949693db
silmidina <silmirhmdn08@gmail.com>
2024-11-16 02:07:03 +0700

    cara pindah browser tab otomatis

--- belajarselenium.py
+++ belajarselenium.py
@@ -13,7 +13,8 @@ driver = webdriver.Chrome(options=chrome_options)
 #driver3= webdriver.Chrome()
 
 #Buka URL
-driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
+driver.get("https://the-internet.herokuapp.com/windows")
+#driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
 #driver.get("https://the-internet.herokuapp.com/login")
 #driver2.get("https://www.facebook.com/")
 #driver3.get("https://www.idejongkok.xyz/")
@@ -24,14 +25,28 @@ driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
 #driver.find_element(By.LINK_TEXT, "Elemental Selenium").click()
 #driver.find_element(By.PARTIAL_LINK_TEXT, "Elemental").click()
 #driver.find_element(By.CLASS_NAME, "radius").click()
-time.sleep(3)
+#time.sleep(3)
 #driver.find_element(By.CSS_SELECTOR, "button.radius").click()
 #driver.find_element(By.CSS_SELECTOR, "#login > button").click()
 #driver.find_element(By.CSS_SELECTOR, "#content > div > button").click()
 
 #menggunakan export XPATH
-driver.find_element(By.XPATH, '//*[@id="content"]/div/button').click()
+#driver.find_element(By.XPATH, '//*[@id="content"]/div/button').click()
 
+#cara pindah browser tab otomatis
+time.sleep(3)
+driver.find_element(By.LINK_TEXT, "Click Here").click()
+time.sleep(3)
+driver.switch_to.window(driver.window_handles[0])
+driver.find_element(By.LINK_TEXT, "Click Here").click()
+time.sleep(3)
+driver.switch_to.window(driver.window_handles[0])
+driver.find_element(By.LINK_TEXT, "Click Here").click()
+time.sleep(3)
+driver.switch_to.window(driver.window_handles[0])
+driver.find_element(By.LINK_TEXT, "Click Here").click()
+time.sleep(3)
+driver.switch_to.window(driver.window_handles[0])
 
 #Untuk menggunakan Tag Name
 #h2 = driver.find_elements(By.TAG_NAME, "h2")
